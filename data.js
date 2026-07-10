07cb4d0fd71f886492fd4a92f70aaa8147158a35
Erinjadexoxo <snymanerin02@gmail.com>
2026-07-10 15:30:07 +0500

    Add wedding details object in data.js

---
+++ data.js
@@ -0,0 +1,57 @@
+const wedding = {
+
+    couple: {
+        groom: "Joshua",
+        bride: "Erin"
+    },
+
+    date: "November 2026",
+
+    ceremony: {
+
+        date: "Coming Soon",
+
+        time: "Coming Soon",
+
+        venue: "Wedding Venue",
+
+        dressCode: "Elegant Celebration"
+
+    },
+
+
+    reception: {
+
+        message:
+        "We invite you to join us as we celebrate this beautiful beginning together."
+
+    },
+
+
+    accommodation: [
+
+        {
+            name: "Hotel Recommendation One",
+            details: "Information coming soon."
+        },
+
+        {
+            name: "Hotel Recommendation Two",
+            details: "Information coming soon."
+        }
+
+    ],
+
+
+    map: {
+
+        link: "#"
+
+    },
+
+
+    closing:
+
+    "We cannot wait to celebrate this unforgettable day with you."
+
+};
